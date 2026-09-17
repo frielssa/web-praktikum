fe2c76fb3e027dbb4025a34c6cc250f6ffa5a439
MizuYzusnn <sanchan135790@gmail.com>
2026-09-17 09:06:31 +0700

    route ticket modification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controller\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/ticket', [TicketController::class, 'index'])
+    ->name('tickets.index');
+
+Route::get('/tickets/{ticket}', [TicketController::class, 'show'])
+    ->whereNumber('ticket')
+    ->name('tickets.show');
+
+Route::get('/api/tickets/{ticket}', [TicketController::class, 'showJson'])
+    ->whereNumber('ticket')
+    ->name('tickets.show-json');
